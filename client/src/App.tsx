031f217119b89d1f0107cf7e1aae06724db0ce66
dmitry <ds@gmail.com>
2022-06-20 14:15:15 +0300

    App routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Routes, Navigate, Route } from "react-router-dom";
 import "./index.scss";
 import Main from "./pages/Main";
 
 const App = () => (
-    <>
-        <Main />
-    </>
+    <Routes>
+        <Route path='/' element={<Navigate to='/main' replace/>} />
+        <Route path='/main' element={<Main />} />  
+    </Routes>
 );
 
 export default App;
